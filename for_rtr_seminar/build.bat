b8ada55c1ef961447c680e1671ec3b6b944be68a
Rutwik Choughule <rutwik110@gmail.com>
2020-03-10 02:02:15 +0530

    build commit

--- for_rtr_seminar/build.bat
+++ for_rtr_seminar/build.bat
@@ -1,5 +1,7 @@
 
-REM copy glew32.dll into C:\system\SysWOW64 folder
+REM copy glew\ folder into C:\ directory
+
+REM copy lib\Release\Win32\glew32.dll from glew directory into C:\system\SysWOW64 directory
 
 cl.exe /c /EHsc /I"C:\glew\include" .\triangle.cpp
 link.exe /LIBPATH:"C:\glew\lib\Release\Win32" triangle.obj opengl32.lib user32.lib kernel32.lib gdi32.lib
